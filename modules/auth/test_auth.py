3334e8d203bd575238164035e4b8add405c8ecd2
Ben Brady <benbradybusiness@gmail.com>
2022-01-05 12:45:56 +0000

    Improved Auth Testing Naming

--- modules/auth/test_auth.py
+++ modules/auth/test_auth.py
@@ -7,13 +7,17 @@ import unittest
 
 
 os.environ['TOKEN_SECRET'] = 'SECRET'
-BAD_PASSWORDS = ['abc','52342','password','123456789','1234567890']
+BAD_PASSWORDS = [
+    'abc',
+    'password',
+    '1234567890',
+]
 GOOD_PASSWORDS = []
 
 getID = lambda:int((random.random()+1)*(10**9))
 getPass = lambda:hex(random.getrandbits(64))
 
-class User(object):
+class Entry(object):
     def __enter__(self,*,ID=None,Pass=None):
         if not ID:
             ID = getID()
@@ -28,18 +32,18 @@ class User(object):
 
 
 class Auth_Create(unittest.TestCase):
-    def test_Creating_User(self):
+    def test_Create_Auth_User(self):
         create(getID(),getPass())
 
 
-    def test_Prevent_Duplicate_Users(self):
-        with User() as (ID,Pass):
+    def test_Prevent_Duplicate_Auth_Entries(self):
+        with Entry() as (ID,Pass):
             try: create(ID,Pass)
             except KeyError: pass
             else: raise KeyError("Allowed duplicate user ID")
 
 
-    def test_Prevent_Creating_With_Bad_Password(self):
+    def test_Prevent_Creating_Entry_With_Bad_Password(self):
         for x in BAD_PASSWORDS:
             try: create(getID(),x)
             except ValueError: pass
@@ -47,13 +51,13 @@ class Auth_Create(unittest.TestCase):
 
 
 class Auth_Login(unittest.TestCase):
-    def test_Login_Accepts_Valid_Passwords(self):
-        with User() as (ID,Pass):
+    def test_Login_Accepts_Correct_Password(self):
+        with Entry() as (ID,Pass):
             login(ID,Pass)
 
 
-    def test_Login_Doesnt_Accept_Invalid_Passwords(self):
-        with User() as (ID,Pass):
+    def test_Login_Doesnt_Accept_Wrong_Passwords(self):
+        with Entry() as (ID,Pass):
             try: login(ID,Pass+"1")
             except LoginFailureError: pass
             else: raise LoginFailureError("Allowed Log in with bad password")
@@ -61,7 +65,7 @@ class Auth_Login(unittest.TestCase):
 
 class Auth_Token(unittest.TestCase):
     def test_Expired_Auth_Tokens_Are_Invalid(self):
-        with User() as (ID,Pass):
+        with Entry() as (ID,Pass):
             token = login(ID,Pass,expiration=0)
             time.sleep(1)
             try: verify(token)
@@ -70,14 +74,14 @@ class Auth_Token(unittest.TestCase):
 
 
     def test_Malformed_Auth_Tokens_Are_Invalid(self):
-        with User() as (ID,Pass):
+        with Entry() as (ID,Pass):
             try: login(ID,Pass,data=[]) #type:ignore PS: For Pylance
             except TypeError: pass
             else: raise TypeError("Allowed Bad Token Payload")
 
 
     def test_Auth_Tokens_From_Deleted_Users_Are_Invalid(self):
-        with User() as (ID,Pass):
+        with Entry() as (ID,Pass):
             token = login(ID,Pass)
             delete(ID)
             try: verify(token)
@@ -86,8 +90,8 @@ class Auth_Token(unittest.TestCase):
 
 
 class Auth_Delete(unittest.TestCase):
-    def test_Users_Are_Deleted_Successfully(self):
-        with User() as (ID,Pass):
+    def test_Entries_Are_Deleted_Successfully(self):
+        with Entry() as (ID,Pass):
             login(ID,Pass)
             delete(ID)
             try: login(ID,Pass)
@@ -96,8 +100,8 @@ class Auth_Delete(unittest.TestCase):
 
 
 class Auth_Password_Change(unittest.TestCase):
-    def test_Updates_The_Password(self):
-        with User() as (ID,Pass):
+    def test_Successfully_Updates_An_Entries_Password(self):
+        with Entry() as (ID,Pass):
             Pass2 = hex(random.getrandbits(64))
             update(ID,Pass,Pass2)
             try: login(ID,Pass)
@@ -106,16 +110,16 @@ class Auth_Password_Change(unittest.TestCase):
             login(ID,Pass2)
 
 
-    def test_Prevents_Changing_To_Bad_Password(self):
-        for x in BAD_PASSWORDS:
-            with User() as (ID,Pass):
-                try: update(ID,Pass,x)
+    def test_Prevents_Changing_Entry_To_Bad_Password(self):
+        for bad_pass in BAD_PASSWORDS:
+            with Entry() as (ID,Pass):
+                try: update(ID,Pass,bad_pass)
                 except ValueError: pass
                 else: raise ValueError("Allowed Bad Password")
 
 
-    def test_Verifes_Old_Password_Is_Valid(self):
-        with User() as (ID,Pass):
+    def test_Change_Password_Verifes_Old_Password(self):
+        with Entry() as (ID,Pass):
             Bad_Pass = "BAD_PASSWORD123"
             Pass2 = hex(random.getrandbits(64))
             try: update(ID,Bad_Pass,Pass2)
