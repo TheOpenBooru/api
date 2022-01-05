cab71cea844f10fdd5717ebc31041221c5b735fb
Ben Brady <benbradybusiness@gmail.com>
2022-01-05 12:41:12 +0000

    Added Validation Module

---
+++ modules/validation.py
@@ -0,0 +1,42 @@
+import re
+import string
+import unittest
+
+class isValid:
+    @staticmethod
+    def name(name:str) -> bool:
+        VALID_CHARS = string.ascii_letters + string.digits + '_'
+        if not(3 < len(name)  < 32):
+            return False # Unacceptable Length
+        elif sum([x not in VALID_CHARS for x in name]):
+            return False # Unacceptable Characters
+        else:
+            return True
+
+    @staticmethod
+    def email(email:str) -> bool:
+        # Taken From https://emailregex.com/
+        REGEX = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        vaid = bool(re.search(REGEX,email))
+        return vaid
+    
+    @staticmethod
+    def md5(md5:str) -> bool:
+        # Taken From https://regexpattern.com/md5-hash/
+        REGEX = r"^([a-f\d]{32}|[A-F\d]{32})$"
+        valid = bool(re.search(REGEX,md5))
+        return valid
+
+class test_Validation(unittest.TestCase):
+    def test_Emails(self):
+        INVALID = [
+            "@example.com"
+            ]
+        VALID = [
+            "david@example.com",
+            "llllll@example.com"
+            ]
+        for invalid in INVALID:
+            self.assertFalse(isValid.email(invalid),invalid)
+        for valid in VALID:
+            self.assertTrue(isValid.email(valid),valid)
