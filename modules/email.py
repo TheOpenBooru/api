dd5fcff61c80fd5fe1be7eb5ddab5f286972cf6a
Ben Brady <benbradybusiness@gmail.com>
2022-01-15 17:23:50 +0000

    Made Template Email Module

---
+++ modules/email.py
@@ -0,0 +1,8 @@
+def reset_password(email:str):
+    raise NotImplementedError
+
+def verify_login(email:str):
+    raise NotImplementedError
+
+def verify_by_email(email:str,text:str):
+    raise NotImplementedError
