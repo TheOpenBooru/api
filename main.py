f4955ed38e2df6ce9d0b0295084d2115920f5614
Ben Brady <benbradybusiness@gmail.com>
2022-01-15 17:26:52 +0000

    Overhauled main.py and removed example_data.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-# from endpoints import .
-
-import fastapi
 import uvicorn
+from fastapi import FastAPI
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
-app = fastapi.FastAPI()
+app = FastAPI(debug=True)
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
-@app.get("/")
-def root():
-    return fastapi.responses.RedirectResponse('/openapi.json')
+from endpoints import auth
+
+@app.get('/auth/captcha_sitekey')
+@limiter.limit("5/minute")
+def get_sitekey(request):
+    return auth.get_sitekey()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=3000, log_level="info",debug=True)
