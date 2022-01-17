064790b2a86f79b5f64a6e3ccade4bd76e1385a1
Ben Brady <benbradybusiness@gmail.com>
2022-01-17 13:52:15 +0000

    Improved Storing Testing

--- test/test_store.py
+++ test/test_store.py
@@ -1,69 +1,76 @@
 """Requirements:
-- It should store data at the key
-- Storing a duplicate key should raise a FileExistsError
-- This data should be retreivable via the key
-- Data should be accessible via a URL
-- Stored Data should not be changed
-- Retrieveing a non-existent key should raise a FileNotFoundError
-- Data should be deletedable via the key
-- You should be able to delete a non-existent key
+- [x] It should store data at the key
+- [x] Storing a duplicate key should raise a FileExistsError
+- [x] Data should be accessible via a URL
+- [X] URL for a non-existent key should raise a FileNotFoundError
+- [X] Getting Data via the key
+- [X] Data should not be changed
+- [X] Getting a non-existent key should raise a FileNotFoundError
+- [] Data should be deletedable via the key
+- [] You should be able to delete a non-existent key
 """
 from modules import store
 import unittest
 import requests
 
-store.delete('example_key')
+
 class test_Store_Data_At_Key(unittest.TestCase):
     def tearDown(self):
         store.delete('example_key')
     def test_Store_Data_At_Key(self):
-        data = b'example_data'
-        store.put('example_key', data)
+        store.put('example_key',  b'example_data')
 
 class test_Duplicate_Keys_Should_Raise_Error(unittest.TestCase):
     def tearDown(self):
         store.delete('example_key')
     def test_Store_Duplicate_Data(self):
-        data = b'example_data'
-        store.put('example_key', data)
-        self.assertRaises(FileExistsError,store.put,'example_key', data)
+        store.put('example_key', b'test')
+        self.assertRaises(FileExistsError,store.put,'example_key', b'test')
 
 class test_URL_Should_Be_Valid(unittest.TestCase):
-    def tearDowN(self):
-        store.delete('example_key_1')
-        store.delete('example_key_2')
+    data = b'example_data'
+    def setUp(self):
+        store.put('example_key', self.data)
+    def tearDown(self):
+        store.delete('example_key')
     def test_URL_Should_Be_Valid(self):
-        store.put('example_key_1', b'example_data')
-        url = store.url('example_key_1')
+        url = store.url('example_key')
         self.assertRegex(url,r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)")
     def test_URL_Should_Reference_Data(self):
-        data = b'test_example_data'
-        store.put('example_key_2', data)
-        url = store.url('example_key_2')
+        url = store.url('example_key')
         r = requests.get(url)
-        self.assertEquals(r.content,data,r.text)
+        self.assertEquals(r.content,self.data,r.text)
 
 
 class test_Data_Is_Retrievable(unittest.TestCase):
-    def setUp(self):
+    def tearDown(self):
         store.delete('example_key')
     def test_Store_Data_At_Key(self):
         data = b'example_data'
         store.put('example_key', data)
         self.assertEquals(data,store.get('example_key'))
 
+class test_Invalid_Key_Should_Raise_Error(unittest.TestCase):
+    def test_Invalid_Get_Key_Should_Raise_Error(self):
+        self.assertRaises(FileNotFoundError,store.get,'non_existent_key')
+    def test_Invalid_URL_Key_Should_Raise_Error(self):
+        self.assertRaises(FileNotFoundError,store.url,'non_existent_key')
 
 class test_Stored_Data_Shouldnt_Be_Changed(unittest.TestCase):
+    data = b'Example\r\nData \n'
     def setUp(self):
+        store.put('example_key', self.data)
+    def tearDown(self):
         store.delete('example_key')
-        store.delete('example_key_crlf')
-    
+    def test_Data_Is_Same(self):
+        store.put('example_key', self.data)
+        self.assertEqual(store.get('example_key'), self.data)
+
+class test_Data_Should_Be_Deletable(unittest.TestCase):
+    def setUp(self):
+        store.put('example_key', data)
+        ...
     def test_Data_Is_Same(self):
         data = b'Example\r\nData \n'
         store.put('example_key', data)
         self.assertEqual(store.get('example_key'), data)
-        
-    def test_CRLF_Shouldnt_Be_Changed(self):
-        data = b'Example\r\nData \n'
-        store.put('example_key_crlf', data)
-        self.assertEqual(store.get('example_key_crlf'), data)
