8210ca5dec29431d08067fc73e3d4a61d14d48f3
Ben Brady <benbradybusiness@gmail.com>
2022-01-25 23:39:54 +0000

    Throw better error on seutp and removed enum

--- modules/database/__init__.py
+++ modules/database/__init__.py
@@ -2,18 +2,15 @@ import os
 import enum
 import neo4j as _neo
 
-class User_Level(enum.IntEnum):
-    TEMPORARY = 0
-    USER = 10
-    ADMIN = 50
-    OWNER = 100
-
-_driver:_neo.BoltDriver =_neo.GraphDatabase.driver(
-    os.getenv('NEO_URI'),
-    auth=(os.getenv('NEO_USER'),os.getenv('NEO_PASS')),
-    connection_acquisition_timeout=5,
-    connection_timeout=3
-    ) # type: ignore
+try:
+    _driver:_neo.BoltDriver =_neo.GraphDatabase.driver(
+        os.getenv('NEO_URI'),
+        auth=(os.getenv('NEO_USER'),os.getenv('NEO_PASS')),
+        connection_acquisition_timeout=5,
+        connection_timeout=3
+        ) # type: ignore
+except Exception:
+    raise Exception("Could not connect to Neo4j, try adding to .env")
 
 
 from .. import Validate
