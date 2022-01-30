935cb4e2b8183c2b6a0e290411a0e34a38fcb960
Ben Brady <benbradybusiness@gmail.com>
2022-01-30 23:27:52 +0000

    Added Placeholder Methods to image

--- modules/image.py
+++ modules/image.py
@@ -1,6 +1,19 @@
 from pathlib import Path
 
-def encode(path:str | Path,target:tuple[int,int] = None) -> str | Path:
+def generateImageVariants(img:bytes) -> tuple[bytes,bytes,bytes]:
+    """Generate downscaled versions of an image
+
+    Args:
+        img (bytes): The Image Data
+
+    Returns:
+        bytes: Full Image
+        bytes: Preview Image
+        bytes: Thumbnail Image
+    """
+    raise NotImplementedError
+
+def encode(img:bytes,target:tuple[int,int] = None) -> bytes:
     """Downscales an image to the target dimensions,
 
     Raises:
