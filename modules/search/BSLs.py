9e4724f760192e574bdbb51deb136688c7d2f8fe
Ben Brady <benbradybusiness@gmail.com>
2022-02-02 16:44:36 +0000

    Removed regex from BSLs

--- modules/search/BSLs.py
+++ modules/search/BSLs.py
@@ -1,4 +1,4 @@
-import regex
+import re
 from . import SearchParameters
 
 defaults = SearchParameters()
@@ -22,7 +22,7 @@ def _parseSort(query: str) -> tuple[str, bool]:
     return defaults.sort, defaults.isAscending
 
 def _parseLimit(query: str) -> int:
-    match = regex.match(r"(?<=limit:)[1-9][0-9]*", query)
+    match = re.match(r"(?<=limit:)[1-9][0-9]*", query)
     if match:
         return int(match.string)
     else:
@@ -30,16 +30,16 @@ def _parseLimit(query: str) -> int:
 
 
 def _parseIncludeTags(query: str) -> list[str]:
-    match = regex.match(r"(?<=^|\s)[a-z0-9_()]*(?=$|\s)", query)
+    match = re.findall(r"(?<=^|\s)[a-z0-9_()]*(?=$|\s)", query)
     if match:
-        return list(set(match.groups()))
+        return list(set(match))
     else:
         return defaults.include_tags
 
 
 def _parseExcludeTags(query: str) -> list[str]:
-    match = regex.match(r"(?<=(^|\s)-)[a-z_()0-9]*", query)
+    match = re.findall(r"(?<=(^|\s)-)[a-z_()0-9]*", query)
     if match:
-        return list(set(match.groups()))
+        return list(set(match))
     else:
-        return defaults.exclude_tags
+        return defaults.include_tags
