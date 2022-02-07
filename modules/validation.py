f1ab2a3f0e270fed959ac94804d1192e278339ff
Ben Brady <benbradybusiness@gmail.com>
2022-02-07 17:23:36 +0000

    Improved Validation

--- modules/validation.py
+++ modules/validation.py
@@ -1,36 +1,52 @@
 import re
 import string
 
-def username(name:str) -> bool:
-    VALID_CHARS = string.ascii_letters + string.digits + '_'
-    if not(3 < len(name) <= 32):
-        return False # Unacceptable Length
+
+def username(name: str) -> bool:
+    VALID_CHARS = string.ascii_letters + string.digits + "_"
+    if not (3 < len(name) <= 32):
+        return False  # Unacceptable Length
     elif sum([x not in VALID_CHARS for x in name]):
-        return False # Unacceptable Characters
+        return False  # Unacceptable Characters
     else:
         return True
 
-def tag(tag:str):
-    VALID_CHARS = string.ascii_letters + string.digits + '_()'
+
+def tag(tag: str) -> bool:
+    VALID_CHARS = string.ascii_letters + string.digits + "_()"
     if len(tag) <= 64:
-        return False # Unacceptable Length
+        return False  # Unacceptable Length
     elif sum([x not in VALID_CHARS for x in tag]):
-        return False # Unacceptable Characters
+        return False  # Unacceptable Characters
     else:
         return True
 
-def url(email:str) -> bool:
+
+def rating(rating:str) -> bool:
+    return rating in {"safe","questionable","explicit"}
+
+
+def language(country_code:str):
+    codes = ["eng","fra"]
+    return country_code in codes
+
+
+
+def url(email: str) -> bool:
     REGEX = r"(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"
-    return bool(re.match(REGEX,email))
+    return bool(re.match(REGEX, email))
 
-def email(email:str) -> bool:
+
+def email(email: str) -> bool:
     REGEX = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-    return bool(re.match(REGEX,email))
-    
-def md5(md5:str) -> bool:
-    REGEX = r"^[0-9a-fA-F]{32}$"
-    return bool(re.match(REGEX,md5))
+    return bool(re.match(REGEX, email))
 
-def sha256(sha:str) -> bool:
+
+def md5(md5: str) -> bool:
+    REGEX = r"^[0-9a-fA-F]{32}$"
+    return bool(re.match(REGEX, md5))
+
+
+def sha256(sha: str) -> bool:
     REGEX = r"^[0-9a-fA-F]{64}$"
-    return bool(re.match(REGEX,sha))
+    return bool(re.match(REGEX, sha))
