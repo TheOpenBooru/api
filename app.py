32cedc665c04c9fff612c89846b966fdae2b9077
Ben Brady <benbradybusiness@gmail.com>
2022-02-07 17:23:11 +0000

    Updated main app file

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
+from modules import settings
+from endpoints import post,tag
+
 import os
 import uvicorn
 from fastapi import FastAPI as _FastAPI
+from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
-from modules import settings
-from endpoints import post,tag,file
 
-import dotenv
-dotenv.load_dotenv()
+app = _FastAPI(
+    version="Alpha",
+    docs_url='/docs',
+    openapi_tags=[
+        {
+            "name":"Unprivileged",
+            "description":"These endpoints can be accessed without an account"}
+        ]
+    )
 
-import logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s; %(name)s; %(levelname)s; %(message)s',
-    filename='/tmp/OpenBooru.log',
-    filemode='a',
+app.add_middleware(CORSMiddleware,
+    allow_origins=["*"],allow_credentials=True,
+    allow_methods=["*"],allow_headers=["*"],
 )
 
-app = _FastAPI()
+@app.get('/',tags=["Unprivileged"])
+def docs_redirect():
+    return RedirectResponse('/docs')
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-app.include_router(post.router,prefix="/post")
-app.include_router(tag.router,prefix="/tag")
-app.include_router(file.router,prefix="/files")
+app.include_router(post.router,prefix="/posts",tags=["Post"])
+app.include_router(tag.router,prefix="/tags",tags=["Tag"])
 
 if __name__ == "__main__":
     uvicorn.run(
