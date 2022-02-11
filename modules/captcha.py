3b4ae9b08c63d097018aaa6fd78944c993efb0e3
Ben Brady <benbradybusiness@gmail.com>
2022-02-11 23:58:36 +0000

    Reorganised captcha module

--- modules/captcha.py
+++ modules/captcha.py
@@ -1,19 +1,21 @@
 from modules import settings
-import os
-import logging
 import requests
+import logging
 
 def sitekey() -> str:
     return settings.get('config.hcaptcha.sitekey')
 
-def verify(response:str) -> bool:
-    r = requests.post("https://hcaptcha.com/siteverify", data={
-        "secret": settings.get('config.hcaptcha.secret'),
-        "response": response
-    })
-    if r.ok:
-        JSON = r.json()
-        return JSON['success']
-    else:
-        logging.warning("Failed to verify captcha:",r.text)
+def verify(captcha_response:str) -> bool:
+    r = requests.post(
+        "https://hcaptcha.com/siteverify",
+        data={
+            "secret": settings.get('config.hcaptcha.secret'),
+            "response": captcha_response
+        }
+    )
+    if not r.ok:
+        logging.error(f"Error while verifying captcha: {r.text}")
         return False
+    else:
+        json = r.json()
+        return json['success']
