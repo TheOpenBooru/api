bdf8f895e8d30f4bb3e94334429620b763b63e8a
Ben Brady <benbradybusiness@gmail.com>
2022-02-18 00:24:05 +0000

    FIxed routing issues

--- endpoints/post/__init__.py
+++ endpoints/post/__init__.py
@@ -1,3 +1,3 @@
 from fastapi import APIRouter
-
-router = APIRouter()
+router = APIRouter(prefix='/posts')
+from . import create,edit,get,delete,search
--- endpoints/tag/__init__.py
+++ endpoints/tag/__init__.py
@@ -1,2 +1,3 @@
 from fastapi import APIRouter
-router = APIRouter()
+router = APIRouter(prefix='/posts')
+from . import all,get
