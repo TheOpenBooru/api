b446cbbe08b941b693b5ef2cd00fb0c7dbbe1a01
Ben Brady <benbradybusiness@gmail.com>
2022-02-19 17:49:52 +0000

    Removed Tag Get

--- endpoints/tag/__init__.py
+++ endpoints/tag/__init__.py
@@ -1,3 +1,3 @@
 from fastapi import APIRouter
 router = APIRouter(prefix="/tags",tags=["Tag"])
-from . import all,get
+from . import all
--- endpoints/tag/get.py
+++
@@ -1,6 +0,0 @@
-from . import router
-from modules import schemas
-
-@router.get('/tag/{name}',response_model=schemas.Tag,tags=["Unprivileged"])
-def get_tag(name:str):
-    ...
