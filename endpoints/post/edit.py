b16d9870cb8fd3ba6ac92786b2e86a66ca75fc9a
Ben Brady <benbradybusiness@gmail.com>
2022-02-22 18:57:19 +0000

    Added Post Edit Endpoint

--- endpoints/post/edit.py
+++ endpoints/post/edit.py
@@ -1,7 +1,11 @@
 from . import router
-from modules import schemas
+from modules import schemas,database
 from fastapi import Response,status
 
 @router.patch('/post/{id}')
 async def edit_post(id:int,new_post_version:schemas.Post):
-    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    if database.Post.get(id=id):
+        database.Post.update(id,new_post_version)
+        return Response(status_code=status.HTTP_205_RESET_CONTENT)
+    else:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
