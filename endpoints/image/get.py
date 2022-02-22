ced6036440312a69dc2271e742344dc7b0590f74
Ben Brady <benbradybusiness@gmail.com>
2022-02-22 17:50:11 +0000

    Return 404 instead of errors in image get

--- endpoints/image/get.py
+++ endpoints/image/get.py
@@ -1,7 +1,12 @@
 from . import router
 from modules import schemas,store
-from fastapi import responses
+from fastapi import responses, status
 
 @router.get('/get/{key}',response_model=bytes)
 def get_image(key:str):
-    return responses.Response(store.get(key))
+    try:
+        data = store.get(key)
+    except KeyError:
+        return responses.Response(status_code=status.HTTP_404_NOT_FOUND)
+    else:
+        return responses.Response(data)
