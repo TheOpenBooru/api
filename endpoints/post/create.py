96c41bc889f63137c20356c562b112dd763db90d
Ben Brady <benbradybusiness@gmail.com>
2022-02-25 18:25:30 +0000

    Reordered post create

--- endpoints/post/create.py
+++ endpoints/post/create.py
@@ -8,6 +8,14 @@ from fastapi import Response,status,UploadFile
 
 @router.post("/create")
 async def create_post(image_file:UploadFile):
+    try:
+        post = _construct_post(image_file)
+        database.Post.create(post)
+        return Response(status_code=status.HTTP_201_CREATED)
+    except ValueError as e:
+        return Response(str(e),status_code=status.HTTP_400_BAD_REQUEST)
+
+async def _construct_post(image_file:UploadFile) -> schemas.Post:
     md5s = []
     sha3_256s = []
     def process_file_hashes(data:bytes):
@@ -16,35 +24,30 @@ async def create_post(image_file:UploadFile):
         sha3 = hashlib.sha3_256(data).hexdigest()
         sha3_256s.append(sha3)
     
-    # image_data:bytes = await image_file.read() # type:ignore
-    # process_file_hashes(image_data)
+    image_data:bytes = await image_file.read() # type:ignore
+    process_file_hashes(image_data)
     
     full_image = image.file_to_image(image_file.file)
-    full = await process_image(full_image)
+    full = await _process_image(full_image)
     process_file_hashes(full_image.data)
     
     preview_image = image.generatePreview(full_image)
-    preview = await process_image(preview_image)
+    preview = await _process_image(preview_image)
     process_file_hashes(preview_image.data)
     
     thumbnail_image = image.generateThumbnail(full_image)
-    thumbnail = await process_image(thumbnail_image)
+    thumbnail = await _process_image(thumbnail_image)
     process_file_hashes(thumbnail_image.data)
 
     postID = database.Post.get_unused_id()
-    post = Post(
+    return Post(
         id=postID,uploader=1,
         sound=False,type='image',
         full=full,preview=preview,thumbnail=thumbnail,
         md5s=md5s,sha256s=sha3_256s,
     )
-    try:
-        database.Post.create(post)
-        return Response(status_code=status.HTTP_201_CREATED)
-    except ValueError as e:
-        return Response(str(e),status_code=status.HTTP_400_BAD_REQUEST)
 
-async def process_image(img:image.Image) -> schemas.Image:
+async def _process_image(img:image.Image) -> schemas.Image:
     key = store.put(img.data,suffix=".webp")
     width,height = img.resolution.to_tuple()
     image_data = schemas.Image(
