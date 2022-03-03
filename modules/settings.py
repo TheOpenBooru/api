457ab0073ec2042027d30e3ee0ad1d26217ef0d6
Ben Brady <benbradybusiness@gmail.com>
2022-03-03 22:02:14 +0000

    Added permentant cache to settings module

--- modules/settings.py
+++ modules/settings.py
@@ -2,14 +2,19 @@ import yaml
 from typing import Any
 from cachetools import cached, TTLCache
 
+@cached(cache=TTLCache(maxsize=1, ttl=10))
+def _load_config() -> dict:
+    with open("./config.yml") as f:
+        config = yaml.full_load(f)
+    return config
 
-@cached(cache=TTLCache(maxsize=2048, ttl=60))
+
+@cached(cache=TTLCache(maxsize=1024, ttl=5))
 def get(setting: str) -> Any:
     """Raises:
     - KeyError: Invalid Setting Name
     """
-    with open("./config.yml") as f:
-        config = yaml.full_load(f)
+    config = _load_config()
     for key in setting.split("."):
         try:
             config = config[key]
