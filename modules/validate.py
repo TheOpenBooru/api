bcac423e7746bae8a38effeed207fb12fc1cbcc0
Ben Brady <benbradybusiness@gmail.com>
2022-03-03 23:20:46 +0000

    Moved Regexes out of their functions

--- modules/validate.py
+++ modules/validate.py
@@ -3,33 +3,31 @@ import iso639 as _iso639
 
 import string as _string
 
-
+USERNAME_REGEX = r"^[a-z0-9_]{4,32}$"
 def username(name: str):
-    REGEX = r"^[a-z0-9_]{4,32}$"
-    if _re.match(REGEX, name):
+    if _re.match(USERNAME_REGEX, name):
         return True
     else:
         return False
 
-
+TAG_REGEX = r"^[a-z0-9_()]{1,64}$"
 def tag(tag: str) -> bool:
-    REGEX = r"^[a-z0-9_()]{1,64}$"
-    if _re.match(REGEX, tag):
+    if _re.match(TAG_REGEX, tag):
         return True
     else:
         return False
 
 
+RATING_REGEX = r"^(safe|questionable|explicit)$"
 def rating(rating:str):
-    REGEX = r"^(safe|questionable|explicit)$"
-    if _re.match(REGEX, rating):
+    if _re.match(RATING_REGEX, rating):
         return True
     else:
         return False
 
+TYPE_REGEX = r"^(image|gif|video)$"
 def post_type(type:str):
-    REGEX = r"^(image|gif|video)$"
-    if _re.match(REGEX, type):
+    if _re.match(TYPE_REGEX, type):
         return True
     else:
         return False
@@ -39,33 +37,33 @@ def language(country_code:str):
     return _iso639.is_valid639_2(country_code)
 
 
+URL_REGEX = r"^(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?$"
 def url(url: str):
-    REGEX = r"^(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?$"
-    if _re.match(REGEX, url):
+    if _re.match(URL_REGEX, url):
         return True
     else:
         return False
 
 
+EMAIL_REGEX = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 def email(email: str):
-    REGEX = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-    if _re.match(REGEX, email):
+    if _re.match(EMAIL_REGEX, email):
         return True
     else:
         return False
 
 
+MD5_REGEX = r"^[0-9a-fA-F]{32}$"
 def md5(md5: str):
-    REGEX = r"^[0-9a-fA-F]{32}$"
-    if _re.match(REGEX, md5):
+    if _re.match(MD5_REGEX, md5):
         return True
     else:
         return False
 
 
+SHA256_REGEX = r"^[0-9a-fA-F]{64}$"
 def sha256(sha: str):
-    REGEX = r"^[0-9a-fA-F]{64}$"
-    if _re.match(REGEX, sha):
+    if _re.match(SHA256_REGEX, sha):
         return True
     else:
         return False
