4b3203d9796d8b03496751cab938a749a8cf2737
Ben Brady <benbradybusiness@gmail.com>
2022-03-09 15:48:07 +0000

    Added transparency test

--- test/test_encoding_animation.py
+++ test/test_encoding_animation.py
@@ -4,10 +4,9 @@ import io
 import unittest
 import asyncio
 from PIL import Image as PILImage
-
+ 
 class TestData:
-    gif = "./data/test/animation/500x500-50ms-12frames.gif"
-    webp = "data/test/animation/500x500-50ms-12frames.webp"
+    transparent = "./data/test/animation/241x300-50ms-128frames.gif"
     single_frame = "data/test/image/SingleFrame.gif"
 
 class OutputLocation:
@@ -18,6 +17,9 @@ def load_animation(path) -> Animation:
     with open(path,'rb') as f:
         return asyncio.run(Animation.from_bytes(f.read()))
 
+def load_PIL_from_data(data) -> PILImage.Image:
+    buf = io.BytesIO(data)
+    return PILImage.open(buf)
 
 class test_Animations_Require_More_Than_One_Frame(unittest.TestCase):
     def test_a(self):
@@ -27,36 +29,48 @@ class test_Animations_Require_More_Than_One_Frame(unittest.TestCase):
         self.assertRaises(ValueError, asyncio.run, coroutine)
 
 
+class test_Animations_Preserve_Transparency(unittest.TestCase):
+    def setUp(self) -> None:
+        self.original_file = TestData.transparent
+        self.animation = load_animation(TestData.transparent)
+        self.full = asyncio.run(self.animation.full())
+    
+    def test_a(self):
+        PIL = load_PIL_from_data(self.full.data)
+        for x in range(PIL.n_frames):
+            PIL.seek(x)
+            MinMax_Channel_Values = PIL.getextrema()
+            _,_,_,(a_min,a_max) = MinMax_Channel_Values
+            assert a_min != 255, f"Frame {x} is not transparent"
+
+
 class test_Animation_Full(unittest.TestCase):
     def setUp(self) -> None:
-        self.original_file = TestData.gif
-        self.animation = load_animation(TestData.gif)
-        self.full = asyncio.run(self.animation.full())
-
-    def load_PIL(self):
-        buf = io.BytesIO(self.full.data)
-        return PILImage.open(buf,formats=None)
+        self.original_file = TestData.transparent
+        self.animation = load_animation(TestData.transparent)
+        coroutine = self.animation.full()
+        self.full = asyncio.run(coroutine)
 
     def test_Is_AnimationFile(self):
         assert isinstance(self.full,AnimationFile), "Did not generate a full version correctly"
     
     def test_Full_Is_Valid(self):
-        PIL = self.load_PIL()
+        PIL = load_PIL_from_data(self.full.data)
         PIL.verify()
     
     def test_Attributes_are_Correct(self):
         PIL = PILImage.open(self.original_file)
-        assert self.full.frame_count == PIL.n_frames, f"Number of frames is not correct: {self.full.frame_count}"
-        assert self.full.duration == 0.6, "File Duration is incorrect"
+        assert self.full.frame_count == 128, f"Number of frames is not correct: {self.full.frame_count}"
+        assert self.full.duration == 6.4, f"File Duration is incorrect: {self.full.duration}"
     
     def test_Save_Example_Image(self):
-        PIL = self.load_PIL()
+        PIL = load_PIL_from_data(self.full.data)
         PIL.save(OutputLocation.full,save_all=True)
 
 
 class test_Animation_Preview(unittest.TestCase):
     def setUp(self) -> None:
-        self.animation = load_animation(TestData.gif)
+        self.animation = load_animation(TestData.transparent)
         self.preview = asyncio.run(self.animation.preview())
 
     def test_Preview_isnt_Generated(self):
@@ -65,19 +79,12 @@ class test_Animation_Preview(unittest.TestCase):
 
 class test_Animation_Thumbnail(unittest.TestCase):
     def setUp(self) -> None:
-        self.animation = load_animation(TestData.gif)
+        self.animation = load_animation(TestData.transparent)
         self.thumbnail = asyncio.run(self.animation.thumbnail())
         self.data = self.thumbnail.data
 
-
-    def load_PIL(self):
-        buf = io.BytesIO(self.data)
-        return PILImage.open(buf,formats=None)
-
-
     def test_Thumbnail_is_ImageFile(self):
         assert isinstance(self.thumbnail,ImageFile), "Did not generate a thumbnail version correctly"
-
 
     def test_Thumbnail_Is_Correct_Resolution(self):
         max_width = settings.get('encoding.image.thumbnail.max_height')
@@ -86,9 +93,9 @@ class test_Animation_Thumbnail(unittest.TestCase):
         assert (width == max_width) or (height == max_height), f"Thumbnail is not the correct resolution: {width}x{height}"
     
     def test_Thumbnail_Is_Valid(self):
-        PIL = self.load_PIL()
+        PIL = load_PIL_from_data(self.data)
         PIL.verify()
 
     def test_Save_Example(self):
-        PIL = self.load_PIL()
+        PIL = load_PIL_from_data(self.data)
         PIL.save(OutputLocation.thumbnail)
