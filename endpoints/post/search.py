9f81fca3f6b8c233cef39af99b46aa7487082265
Ben Brady <benbradybusiness@gmail.com>
2022-03-22 15:52:04 +0000

    Fixed bad json response

--- endpoints/post/search.py
+++ endpoints/post/search.py
@@ -1,10 +1,15 @@
 from . import router
 from modules import schemas,search
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 @router.get("/search", response_model=list[schemas.Post])
 async def search_posts(query:str=""):
     CACHE_HEADER = {"Cache-Control": "max-age=60, public"}
     search_settings = search.parseBSLs(query)
     posts = search.searchPosts(search_settings)
-    return JSONResponse(content=posts, headers=CACHE_HEADER)
+    json = jsonable_encoder(posts)
+    return JSONResponse(
+        content=json,
+        headers=CACHE_HEADER,
+    )
