f92b24b039e1869e60af8a64a120b2d343b0b3f4
Ben Brady <benbradybusiness@gmail.com>
2022-03-22 14:08:01 +0000

    Added image cache

--- endpoints/image/get.py
+++ endpoints/image/get.py
@@ -4,9 +4,12 @@ from fastapi import responses, status
 
 @router.get('/get/{key}',response_model=bytes)
 def get_image(key:str):
+    CACHE_HEADER = {
+        "Cache-Control": "max-age=31536000, public"
+    }
     try:
         data = store.get(key)
     except FileNotFoundError:
         return responses.Response(status_code=status.HTTP_404_NOT_FOUND)
     else:
-        return responses.Response(data)
+        return responses.Response(data,headers=CACHE_HEADER)
