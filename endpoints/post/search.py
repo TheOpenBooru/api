e68afd9b681fbc7a2fe5edb3e5b74530211e805b
Ben Brady <benbradybusiness@gmail.com>
2022-03-22 15:04:17 +0000

    added Cache Control

--- endpoints/post/search.py
+++ endpoints/post/search.py
@@ -1,8 +1,12 @@
 from . import router
 from modules import schemas,search
+from fastapi import Response
 
 @router.get("/search", response_model=list[schemas.Post])
 async def search_posts(query:str=""):
+    CACHE_HEADER = {
+        "Cache-Control": "max-age=60, public"
+    }
     search_settings = search.parseBSLs(query)
     posts = search.searchPosts(search_settings)
-    return posts
+    return Response(posts,headers=CACHE_HEADER)
