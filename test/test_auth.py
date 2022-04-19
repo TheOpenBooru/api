b298e683ccf7346d277710ef104aec3312448d85
Ben Brady <benbradybusiness@gmail.com>
2022-04-19 03:38:46 +0100

    Updated auth tests

--- test/test_auth.py
+++ test/test_auth.py
@@ -9,14 +9,19 @@ VALID_PASSWORD = r"GzsU`d>%8r_WK`u~JpMZR}tmL{,#:vmC'__\BTw#x2aVq+\Q{)"
 class test_cant_register_user_twice(unittest.TestCase):
     def setUp(self):
         auth.register('user1',VALID_PASSWORD)
+    
     def tearDown(self):
         auth.delete('user1')
+    
     def test_cant_register_user_twice(self):
         with self.assertRaises(KeyError):
             auth.register('user1',VALID_PASSWORD)
 
 
 class test_Register_and_Signin(unittest.TestCase):
+    def tearDown(self):
+        auth.delete('user1')
+    
     def test_Register_and_Signin(self):
         auth.register('user1',VALID_PASSWORD)
         assert auth.login('user1',VALID_PASSWORD)
@@ -24,40 +29,25 @@ class test_Register_and_Signin(unittest.TestCase):
         auth.delete('user1')
         assert auth.login('user1',VALID_PASSWORD) == False
 
-class test_Login_is_Timing_Attack_Safe(unittest.TestCase):
-    def setUp(self):
-        auth.register('user1',VALID_PASSWORD)
-    def tearDown(self):
-        auth.delete('user1')
-
-    def measure_login_speed(self,iterations,password):
-        start = time.time()
-        for _ in range(iterations):
-            auth.login('user1',password)
-        duration = time.time() - start
-        return duration
-    
-    def test_Login_is_Timing_Attack_Safe(self):
-        TEST_DURATION = 5
-        ACCEPTABLE_PERCENTAGE = 10
-        
-        hashrate = self.measure_login_speed(20,VALID_PASSWORD)
-        iterations = int(TEST_DURATION / (hashrate / 2))
-        valid_duration = self.measure_login_speed(iterations,VALID_PASSWORD)
-        invalid_duration = self.measure_login_speed(iterations,"abc")
-        delta_percent = valid_duration / invalid_duration
-
-        self.assertAlmostEqual(
-            first=delta_percent,second=1,
-            delta=(0.01 * ACCEPTABLE_PERCENTAGE),
-        )
-
 
 class test_Register_and_Delete(unittest.TestCase):
     def tearDown(self):
         auth.delete('user1')
     
+    
     def test_Register_and_Signin(self):
         auth.register('user1',VALID_PASSWORD)
         assert auth.login('user1',VALID_PASSWORD)
         assert auth.login('user1','abc') == False
+
+class test_Password_Changes_Updates_Password(unittest.TestCase):
+    def setUp(self):
+        auth.register('user1',VALID_PASSWORD)
+    def tearDown(self):
+        auth.delete('user1')
+    
+    def test_Register_and_Signin(self):
+        new_password = VALID_PASSWORD + 'a'
+        auth.change_password('user1',new_password)
+        assert auth.login('user1',VALID_PASSWORD) == False
+        assert auth.login('user1',new_password) == True
