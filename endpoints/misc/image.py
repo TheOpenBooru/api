030109ba06febf81bcc1da36baba625c45270223
Ben Brady <benbradybusiness@gmail.com>
2022-04-24 11:49:51 +0100

    Update store endpoint to support other methods

--- endpoints/misc/image.py
+++ endpoints/misc/image.py
@@ -1,16 +1,19 @@
-from modules import store
 from .. import router
-from modules import settings
-from fastapi import responses, status
+from modules import store, settings
+from fastapi import Response, responses, status
 
-@router.get('/image/{key}',status_code=200,tags=["Misc"])
-def get_image(key:str):
+@router.get('/image/{key}',status_code=200,include_in_schema=False)
+def get_image(file:str):
     CACHE_HEADER = {
         "Cache-Control": "max-age=31536000, public"
     }
-    try:
-        data = store.get(key)
-    except FileNotFoundError:
-        return responses.Response(status_code=status.HTTP_404_NOT_FOUND)
+    if settings.STORAGE_METHOD == "LOCAL":
+        try:
+            data = store.get(file)
+        except FileNotFoundError:
+            return Response(status_code=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(data,headers=CACHE_HEADER)
     else:
-        return responses.Response(data,headers=CACHE_HEADER)
+        url = store.url(file)
+        return responses.RedirectResponse(url=url)
