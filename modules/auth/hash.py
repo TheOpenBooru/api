fa47153aaed6d5113a9f72305cf023f7d5cf2ad6
Ben Brady <benbradybusiness@gmail.com>
2022-05-06 13:47:00 +0100

    Added return annotations to hash methods

--- modules/auth/hash.py
+++ modules/auth/hash.py
@@ -1,7 +1,7 @@
 from passlib.hash import argon2
 
-def hash(password:str):
+def hash(password:str) -> str:
     return argon2.hash(password)
 
-def compare(password:str,hash:str):
+def compare(password:str,hash:str) -> bool:
     return argon2.verify(password,hash)
