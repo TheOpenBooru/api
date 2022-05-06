793e6e30a309832da25eeed14b343405ca35ac6b
Ben Brady <benbradybusiness@gmail.com>
2022-05-06 13:48:23 +0100

    captcha test doesnt run if it can't connect

--- test/test_captcha.py
+++ test/test_captcha.py
@@ -1,16 +1,28 @@
+from modules import captcha,settings
 import unittest
-from modules import captcha
+import requests
 
-test_key = "10000000-aaaa-bbbb-cccc-000000000001"
-test_secret = "0x0000000000000000000000000000000000000000"
-test_response = "10000000-aaaa-bbbb-cccc-000000000001"
+TEST_KEY = "10000000-aaaa-bbbb-cccc-000000000001"
+TEST_SECRET = "0x0000000000000000000000000000000000000000"
+TEST_RESPONSE = "10000000-aaaa-bbbb-cccc-000000000001"
 
-class test_captcha_verify(unittest.TestCase):
+try:
+    requests.get("https://hcaptcha.com/siteverify",timeout=1)
+except Exception:
+    CAN_CONNECT = False
+else:
+    CAN_CONNECT = True
+
+@unittest.skipUnless(CAN_CONNECT,"Cannot contact hcaptcha api")
+class TestCase(unittest.TestCase):
+    ...
+
+class test_captcha_verify(TestCase):
     def test_verifies_valid_responses(self):
-        success = captcha.verify(test_response, test_secret)
+        success = captcha.verify(TEST_RESPONSE, TEST_SECRET)
         assert success, "Didn't validate test key"
         
     def test_denies_valid_responses(self):
         bad_response = "12345678-abcd-abcd-abcd-123456789012"
-        success = captcha.verify(bad_response, test_secret)
+        success = captcha.verify(bad_response, TEST_SECRET)
         assert success == False, "Validated bad response"
