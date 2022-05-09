694f8beea86b190a52eea2ef5f243a89dd577101
Ben Brady <benbradybusiness@gmail.com>
2022-05-09 12:45:43 +0100

    Auth database now uses sqlite

--- modules/auth/database.py
+++ modules/auth/database.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+import sqlite3
+
 
 @dataclass()
 class User:
@@ -6,19 +8,57 @@ class User:
     hash:str
     secret_2fa:str|None = None
 
-users = {}
+
+conn = sqlite3.connect('./data/auth.db')
+conn.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY,
+        hash TEXT,
+        secret_2fa TEXT
+    );
+""")
 
 def create(user:User):
-    users[user.username] = user
+    with conn:
+        conn.execute(
+            "INSERT INTO users (username,hash,secret_2fa) VALUES (?,?,?);",
+            (user.username,user.hash,user.secret_2fa)
+        )
 
 def update_hash(username:str,hash:str):
-    users[username].hash = hash
+    with conn:
+        conn.execute(
+            "UPDATE users SET hash=? WHERE username=?;",
+            (hash,username)
+        )
 
 def update_secret(username:str,secret:str):
-    users[username].secret_2fa = secret
+    with conn:
+        conn.execute(
+            "UPDATE users SET secret_2fa=? WHERE username=?;",
+            (secret,username)
+        )
 
 def get(username:str) -> User | None:
-    return users.get(username,None)
+    with conn:
+        cursor = conn.execute(
+            "SELECT hash,secret_2fa FROM users WHERE username=?;",
+            (username,)
+        )
+    data = cursor.fetchone()
+    if data is None:
+        return None
+    else:
+        hash,secret = data
+        return User(
+            username=username,
+            hash=hash,
+            secret_2fa=secret
+        )
 
 def delete(username:str):
-    users.pop(username,None)
+    with conn:
+        conn.execute(
+            "DELETE FROM users WHERE username=?;",
+            (username,)
+        )
