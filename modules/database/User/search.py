bdea1dc89ab3714358a6168359f63f3532d89b2f
Ben Brady <benbradybusiness@gmail.com>
2022-05-09 12:36:42 +0100

    Implemented a basic user search

--- modules/database/User/search.py
+++ modules/database/User/search.py
@@ -1,4 +1,13 @@
 from . import User,user_collection
+from modules.schemas import User_Query
 
-def search(query) -> list[User]:
-    raise NotImplementedError
+def search(query:User_Query) -> list[User]:
+    if query.username:
+        cursor = user_collection.find({"username":query.username})
+    elif query.email:
+        cursor = user_collection.find({'email':query.email})
+    else:
+        cursor = user_collection.find({})
+    
+    users = [User.parse_obj(doc) for doc in cursor]
+    return users
