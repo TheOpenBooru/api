ae9ef49ea9a7bffc321f844fc2296bd26b258891
Ben Brady <benbradybusiness@gmail.com>
2022-06-01 17:36:51 +0100

    Upgraded Status Endpoint

--- endpoints/misc/status.py
+++ endpoints/misc/status.py
@@ -1,8 +1,15 @@
 from . import router
-from modules import schemas
+from modules import schemas,settings
 
 @router.get('/status',response_model=schemas.misc.Status)
 def get_status():
     return {
-        "online": True
+        "online": True,
+        "config": {
+            "DefaultSort":settings.POSTS_SEARCH_DEFAULT_SORT,
+            "SearchLimit":settings.POSTS_SEARCH_MAX_LIMIT,
+            "SiteName":settings.SITE_NAME,
+            "Hostname":settings.HOSTNAME,
+            "Port":settings.PORT,
+        }
     }
