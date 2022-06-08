1a72f16134c32c871395a3dcba9dadb8fac95b8f
Ben Brady <benbradybusiness@gmail.com>
2022-06-08 13:03:05 +0100

    Added Type Enum to Media

--- modules/schemas/media.py
+++ modules/schemas/media.py
@@ -1,26 +1,34 @@
-from pydantic import Field
+from typing import Union
 from . import BaseModel
+from pydantic import Field
+from enum import Enum
+
+class Type(str, Enum):
+    image = 'image'
+    video = 'video'
+    animation = 'video'
 
 class BaseMedia(BaseModel):
     url: str = Field(..., description="The URI for the File")
     mimetype: str = Field(..., description="The Media's Mimetype")
     height: int = Field(..., description="The Media's Height in pixels")
     width: int = Field(..., description="The Media's Width in pixels")
+    type: Type
 
 class Image(BaseMedia):
-    type: str = Field(default="image", description="The type of media")
+    type: Type = Field(default="image", description="The type of media")
 
 
 class Animation(BaseMedia):
     duration: float = Field(..., description="The Animation's Duration in framerate")
     frame_count: int = Field(..., description="The Animation's Number of frames")
     duration:float = Field(..., description="The Animation's Duration")
-    type:str = Field(default="animation", description="The type of media")
+    type:Type = Field(default="animation", description="The type of media")
 
 class Video(BaseMedia):
     has_sound: bool = Field(..., description="Does the video contain sound?")
     duration: float = Field(..., description="The Video's Duration in framerate")
     fps: str = Field(..., description="The Video's Framerate in frames per second")
-    type = Field(default="video", description="The type of media")
+    type:Type = Field(default="video", description="The type of media")
 
-GenericMedia = Image | Animation | Video
+GenericMedia = Union[Image,Animation,Video]
