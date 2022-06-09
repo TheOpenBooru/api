3c859ef016d597a9f2f86bf79b8205e07aa8cc6b
Ben Brady <benbradybusiness@gmail.com>
2022-06-09 14:01:38 +0100

    Added GetByUsername method to User

--- modules/database/User/__init__.py
+++ modules/database/User/__init__.py
@@ -3,7 +3,7 @@ from modules.schemas import User
 from .. import db
 from ._collection import user_collection
 from .create import create
-from .get import get
+from .get import get,getByUsername
 from .search import search
 from .exists import exists
 from .delete import delete
--- modules/database/User/get.py
+++ modules/database/User/get.py
@@ -10,3 +10,14 @@ def get(id:int) -> User:
     else:
         user = User.parse_obj(document)
         return user
+
+def getByUsername(username:str) -> User:
+    """Raises:
+    KeyError: User does not exist
+    """
+    document = user_collection.find_one({'username':username})
+    if document == None:
+        raise KeyError("User does not exist")
+    else:
+        user = User.parse_obj(document)
+        return user
