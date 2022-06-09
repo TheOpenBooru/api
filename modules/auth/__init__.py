2d6c58be8b25ea6d110ea5e1f14e5632f669a811
Ben Brady <benbradybusiness@gmail.com>
2022-06-09 14:28:45 +0100

    Reduced Auth Readme

--- modules/auth/__init__.py
+++ modules/auth/__init__.py
@@ -1,6 +1,3 @@
-"""All security should be encapsulated in this module
-Including:
-- Timing Attacks
-"""
+"""All security should be encapsulated in this module"""
 from .user import register,delete,exists,login,change_password
 from .password import is_password_valid,get_password_requirements
