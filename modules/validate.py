6a9b416538ea6212cea790a3f2249833f81867cb
Ben Brady <benbradybusiness@gmail.com>
2022-06-13 20:19:25 +0100

    Reorganised Validate

--- modules/validate.py
+++ modules/validate.py
@@ -3,47 +3,46 @@ import iso639 as _iso639
 from modules import settings
 
 USERNAME_REGEX = r"^[a-z0-9_]{4,32}$"
-def username(name: str):
-    if _re.match(USERNAME_REGEX, name):
-        return True
-    else:
-        return False
+RATING_REGEX = r"^(safe|questionable|explicit)$"
+TYPE_REGEX = r"^(image|animation|video)$"
+TAG_REGEX = r"^([a-z]{1,32}:)?[_()a-z0-9]{1,32}$"
+URL_REGEX = r"^http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+$"
+EMAIL_REGEX = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+MD5_REGEX = r"^[0-9a-fA-F]{32}$"
+SHA256_REGEX = r"^[0-9a-fA-F]{64}$"
 
 
-def language(country_code:str):
+def language(country_code:str) -> bool:
     return _iso639.is_valid639_2(country_code)
 
 
-RATING_REGEX = r"^(safe|questionable|explicit)$"
-def rating(rating:str):
+def username(name: str) -> bool:
+    return bool(_re.match(USERNAME_REGEX, name))
+
+
+def rating(rating:str) -> bool:
     return bool(_re.match(RATING_REGEX, rating))
 
 
-TYPE_REGEX = r"^(image|animation|video)$"
-def post_type(type:str):
+def post_type(type:str) -> bool:
     return bool(_re.match(TYPE_REGEX, type))
 
 
-TAG_REGEX = r"^([a-z]{1,32}:)?[_()a-z0-9]{1,32}$"
 def tag(tag: str) -> bool:
     return bool(_re.match(TAG_REGEX, tag))
 
 
-URL_REGEX = r"^http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+$"
-def url(url: str):
+def url(url: str) -> bool:
     return bool(_re.match(URL_REGEX, url))
 
 
-EMAIL_REGEX = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-def email(email: str):
+def email(email: str) -> bool:
     return bool(_re.match(EMAIL_REGEX, email))
 
 
-MD5_REGEX = r"^[0-9a-fA-F]{32}$"
-def md5(md5: str):
+def md5(md5: str) -> bool:
     return bool(_re.match(MD5_REGEX, md5))
 
 
-SHA256_REGEX = r"^[0-9a-fA-F]{64}$"
-def sha256(sha: str):
+def sha256(sha: str) -> bool:
     return bool(_re.match(SHA256_REGEX, sha))
