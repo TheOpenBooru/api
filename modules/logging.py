0352db88245e1f6c1ac35540cccfe3cccf1a64fd
Ben Brady <benbradybusiness@gmail.com>
2022-06-27 16:06:49 +0100

    Improved Logging

--- modules/logging.py
+++ modules/logging.py
@@ -1,9 +1,20 @@
 import logging
+import datetime
+import coloredlogs
 
 
-logging.basicConfig(
-    filename='./data/log.txt',
-    filemode='a',
-    level=logging.DEBUG,
-    format='[%(asctime)s] %(levelname)s - %(message)s',
-)
+CUR_TIME = datetime.datetime.now().strftime("%m %d %Y-%H:%M:%S")
+logger = logging.getLogger()
+
+stream_handler = logging.StreamHandler()
+file_handler = logging.FileHandler(f"./data/logs/{CUR_TIME}.log")
+stream_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("[%(asctime)s] %(levelname)s (%(name)s): %(message)s")
+stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+coloredlogs.install(level=logging.INFO,logger=logger)
