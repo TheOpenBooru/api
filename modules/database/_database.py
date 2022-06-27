b808909b795259a1c068752241ac5190e829f0e9
Ben Brady <benbradybusiness@gmail.com>
2022-06-27 11:29:18 +0100

    Fixed mongodb login

--- modules/database/_database.py
+++ modules/database/_database.py
@@ -1,12 +1,20 @@
 from modules import settings
 from pymongo.mongo_client import MongoClient as _MongoClient
 
-db_client = _MongoClient(
+if settings.MONGODB_USERNAME:
+    TEMPLATE_URL = "mongodb://{user}@{host}:{port}"
+elif settings.MONGODB_USERNAME and settings.MONGODB_PASSWORD:
+    TEMPLATE_URL = "mongodb://{user}:{_pass}@{host}:{port}"
+else:
+    TEMPLATE_URL = "mongodb://{host}:{port}"
+
+url = TEMPLATE_URL.format(
     host=settings.MONGODB_HOSTNAME,
     port=settings.MONGODB_PORT,
-    username=settings.MONGODB_USERNAME,
-    password=settings.MONGODB_PASSWORD,
+    user=settings.MONGODB_USERNAME,
+    _pass=settings.MONGODB_PASSWORD,
 )
+db_client = _MongoClient(url)
 
 if settings.WIPE_ON_STARTUP:
     db_client.drop_database(settings.MONGODB_DB_NAME)
