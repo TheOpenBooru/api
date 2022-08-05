8796636ab3ea3b9158d5518268d124e584887f95
Ben Brady <benbradybusiness@gmail.com>
2022-08-05 22:31:51 +0100

    Removed Eronousely Error Warning in Post Exists

--- modules/database/Post/exists.py
+++ modules/database/Post/exists.py
@@ -1,27 +1,15 @@
 from . import post_collection
 
 def exists(id:int) -> bool:
-    """Raises
-    - KeyError: Could not find post
-    """
     return _exists_by_filter({'id':id})
 
 def md5_exists(md5:str) -> bool:
-    """Raises
-    - KeyError: Could not find post
-    """
     return _exists_by_filter({"hashes.md5s":{'$elemMatch':{"$eq":md5}}})
 
 def sha256_exists(sha256:str) -> bool:
-    """Raises
-    - KeyError: Could not find post
-    """
     return _exists_by_filter({"hashes.sha256s":{'$elemMatch':{"$eq":sha256}}})
 
 def source_exists(source:str) -> bool:
-    """Raises
-    - KeyError: Could not find post
-    """
     return _exists_by_filter({"source":{'$elemMatch':{"$eq":source}}})
 
 def _exists_by_filter(filter: dict) -> bool:
