8c34d0ab11fd3510963299d266545f7c6053ecd0
Ben Brady <benbradybusiness@gmail.com>
2022-08-26 22:28:49 +0100

    Added caching to validate

--- modules/validate.py
+++ modules/validate.py
@@ -1,3 +1,4 @@
+from functools import cache
 import re as _re
 import iso639 as _iso639
 from modules import settings
@@ -12,37 +13,46 @@ MD5_REGEX = r"^[0-9a-fA-F]{32}$"
 SHA256_REGEX = r"^[0-9a-fA-F]{64}$"
 
 
+@cache
 def language(country_code:str) -> bool:
     return _iso639.is_valid639_2(country_code)
 
 
+@cache
 def username(name: str) -> bool:
     return bool(_re.match(USERNAME_REGEX, name))
 
 
+@cache
 def rating(rating:str) -> bool:
     return bool(_re.match(RATING_REGEX, rating))
 
 
+@cache
 def post_type(type:str) -> bool:
     return bool(_re.match(TYPE_REGEX, type))
 
 
+@cache
 def tag(tag: str) -> bool:
     return bool(_re.match(TAG_REGEX, tag))
 
 
+@cache
 def url(url: str) -> bool:
     return bool(_re.match(URL_REGEX, url))
 
 
+@cache
 def email(email: str) -> bool:
     return bool(_re.match(EMAIL_REGEX, email))
 
 
+@cache
 def md5(md5: str) -> bool:
     return bool(_re.match(MD5_REGEX, md5))
 
 
+@cache
 def sha256(sha: str) -> bool:
     return bool(_re.match(SHA256_REGEX, sha))
