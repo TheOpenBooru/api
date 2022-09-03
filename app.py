e512feceebc1d470d1ed6be059d455900ff7e2f9
Ben Brady <benbradybusiness@gmail.com>
2022-09-03 17:42:03 +0100

    Removed debug from Uvicorn init

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host='0.0.0.0',
-        debug=True,
         port=settings.PORT,
         log_config="./data/logging.conf",
         **ssl_params,
