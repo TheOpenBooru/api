e1810ae0f87ce346eeeacce8409e954699f3c913
Ben Brady <benbradybusiness@gmail.com>
2022-09-10 16:32:32 +0100

    Twitter downloader can now download individual

--- modules/downloaders/sites/twitter.py
+++ modules/downloaders/sites/twitter.py
@@ -1,3 +1,4 @@
+from typing import Union
 from . import Downloader, DownloadFailure, utils
 from modules import settings, posts, schemas
 import re
@@ -22,49 +23,74 @@ class Twitter(Downloader):
 
     ACCOUNT_REGEX = r"^https:\/\/twitter.com\/[a-zA-Z0-9_]+"
     ID_REGEX = r"^https:\/\/twitter.com\/[a-zA-Z0-9_]*\/status\/[0-9]+"
+    IMAGE_REGEX = r"^^https:\/\/twitter.com\/[a-zA-Z0-9_]*\/status\/[0-9]+\/photo\/[0-9]+"
     async def download_url(self, url:str) -> list[schemas.Post]:
         account_match = re.match(self.ACCOUNT_REGEX, url)
         id_match = re.match(self.ID_REGEX, url)
+        image_match = re.match(self.IMAGE_REGEX, url)
         
         if id_match and account_match:
             id = id_match.group().split('/')[-1]
             account = account_match.group().split('/')[-1]
-            posts = await self.import_tweet(id, url, account)
-            return posts
-        elif account_match:
-            raise DownloadFailure("Importing Twitter Accounts not Supported, Please Import Single Tweets")
+            
+            if image_match:
+                index_string = image_match.group().split('/')[-1]
+                photo_index = int(index_string) - 1
+                return await self.import_tweet(id, url, account, photo_index)
+            else:
+                return await self.import_tweet(id, url, account)
         else:
             raise DownloadFailure("Could Not Import Twitter URL")
 
-    async def import_tweet(self, id:str, url:str, account:str) -> list[schemas.Post]:
+
+    async def import_tweet(self, id:str, url:str, account:str, index:Union[int,None] = None) -> list[schemas.Post]:
         tweet = self.client.get_tweet(
             id=id,
             expansions="attachments.media_keys",
             media_fields="url",
             tweet_fields="attachments,author_id",
         )
-        
-        if 'media' not in tweet.includes:
-            return []
+        if "media" not in tweet.includes:
+            raise DownloadFailure("Post Doesn't Have Media")
+
+        medias = tweet.includes["media"]
+        if index:
+            media = medias[index]
+            post = await generate_from_media(media, url, account)
+            return [post]
         else:
-            account = account.lower()
             new_posts = []
-            for media in tweet.includes['media']:
-                if media.type == "video":
-                    continue # Videos need to be handled differently
-                if media.type == "animated_gif":
-                    continue # Gifs need to be handled differently
-                else:
-                    post = await generate_image(media)
-                post.source = url
-                if account not in post.tags:
-                    post.tags.append(account)
+            for media in medias:
+                post = await generate_from_media(media, url, account)
                 new_posts.append(post)
-            
+
             return new_posts
+
+
+async def generate_from_media(media: tweepy.Media, url:str, account:str) -> schemas.Post:
+    if media.type == "video":
+        post = await generate_video(media)
+    elif media.type == "animated_gif":
+        post = await generate_gif(media)
+    else:
+        post = await generate_image(media)
+
+    post.source = url
+    if account not in post.tags:
+        post.tags.append(account.lower())
+
+    return post
 
 
 async def generate_image(media: tweepy.Media) -> schemas.Post:
     data, filename = utils.download_url(media["url"])
     post = await posts.generate(data,filename)
     return post
+
+
+async def generate_gif(media: tweepy.Media) -> schemas.Post:
+    raise DownloadFailure("Twitter Gifs Not Supported")
+
+
+async def generate_video(media: tweepy.Media) -> schemas.Post:
+    raise DownloadFailure("Twitter Video Not Supported")
--- test/downloaders/test_twitter.py
+++ test/downloaders/test_twitter.py
@@ -7,7 +7,7 @@ def twitter():
     return downloaders.Twitter()
 
 
-@pytest.mark.skip("Not Implemented")
+@pytest.mark.skip("Not Implemented: https://trello.com/c/2g3ymGhJ/79-bug-fix-twitter-video")
 @pytest.mark.asyncio
 async def test_Importing_Video_Tweet(twitter: downloaders.Twitter):
     tweet = "https://twitter.com/OpenBooru/status/1537464462063677441?s=20&t=oInfaI6U8aQCNJDivAK4mQ"
@@ -18,6 +18,19 @@ async def test_Importing_Video_Tweet(twitter: downloaders.Twitter):
     assert post.media_type == "video"
     assert post.source == tweet
     assert "openbooru" in post.tags
+
+
+@pytest.mark.skip("Not Implemented: https://trello.com/c/ITCodFP4/93-bug-fix-twitter-animation")
+@pytest.mark.asyncio
+async def test_Importing_Animation_Tweet(twitter: downloaders.Twitter):
+    tweet = "https://twitter.com/LykeIsland/status/1568355135012626435"
+    posts = await twitter.download_url(tweet)
+    
+    assert len(posts) == 1
+    post = posts[0]
+    assert post.media_type == "animation"
+    assert post.source == tweet
+    assert "lykeisland" in post.tags
 
 
 @pytest.mark.asyncio
@@ -32,12 +45,26 @@ async def test_Importing_Image_Tweet(twitter: downloaders.Twitter):
     assert "advosart" in post.tags
 
 
+# @pytest.mark.skip("Not Implemented: https://trello.com/c/6ezJteGU/80-bug-fix-twitter-individual-image")
 @pytest.mark.asyncio
-async def test_Importing_Text_Tweet(twitter: downloaders.Twitter):
-    tweet = "https://twitter.com/OpenBooru/status/1552791748925161472?s=20"
+async def test_Importing_Individual_Image(twitter: downloaders.Twitter):
+    tweet = "https://twitter.com/OpenBooru/status/1566759990681026562/photo/2"
     posts = await twitter.download_url(tweet)
     
-    assert len(posts) == 0
+    assert len(posts) == 1
+    post = posts[0]
+    assert post.media_type == "image"
+    assert post.source == tweet
+    assert post.full.width == 1920
+    assert post.full.height == 927
+    assert "openbooru" in post.tags
+
+
+@pytest.mark.asyncio
+async def test_Importing_Text_Tweet(twitter: downloaders.Twitter):
+    tweet = "https://twitter.com/OpenBooru/status/1552791748925161472"
+    with pytest.raises(downloaders.DownloadFailure):
+        await twitter.download_url(tweet)
 
 
 @pytest.mark.asyncio
