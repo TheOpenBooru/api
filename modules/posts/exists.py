f8cb21438c139cdcec34fcca06f6908b9439886b
Ben Brady <benbradybusiness@gmail.com>
2022-09-20 13:56:07 +0100

    Fixed using hexdigest instead of digest

--- modules/posts/exists.py
+++ modules/posts/exists.py
@@ -2,8 +2,8 @@ from modules import database
 import hashlib
 
 def exists_hash(data:bytes) -> bool:
-    md5 = hashlib.md5(data).hexdigest()
-    sha256 = hashlib.sha256(data).hexdigest()
+    md5 = hashlib.md5(data).digest()
+    sha256 = hashlib.sha256(data).digest()
     return any([
         database.Post.md5_exists(md5),
         database.Post.sha256_exists(sha256),
