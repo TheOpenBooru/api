7b14cad4891db62098879bd2c51b5e3e8c37f54b
Ben Brady <benbradybusiness@gmail.com>
2022-09-30 21:43:12 +0100

    Updated Daemon

--- modules/daemon/daemon.py
+++ modules/daemon/daemon.py
@@ -7,40 +7,34 @@ import logging
 
 def run_daemon():
     logging.info("Starting Daemon Threads")
+
     if settings.TAGS_REGEN_COUNT_EVERY:
         create_background_task(
             id="Regen Tag Count",
             function=tags.regenerate_count,
             retry_after=settings.TAGS_REGEN_COUNT_EVERY,
         )
+    if settings.TAGS_IMPORT_TAG_DATA_EVERY:
         create_background_task(
-            id="Updated Namespaces",
-            function=tags.regenerate_namespaces,
-            # e621 namespace dumps only regen every day
-            retry_after=timedelta(days=1).total_seconds(), 
+            id="Regen Tag Count",
+            function=tags.import_e621_tag_data,
+            retry_after=settings.TAGS_IMPORT_TAG_DATA_EVERY,
         )
-    schedule_importers()
 
-
-def schedule_importers():
+    
     importers = [Hydrus, Files, Rule34, Safebooru, E621]
-    try:
-        for importer_class in importers:
-            name = importer_class.__name__
-            
-            if importer_class.enabled == False:
-                continue
+    for importer_class in importers:
+        if importer_class.enabled == False:
+            continue
 
-            try:
-                importer = importer_class()
-            except Exception:
-                logging.warning(f"Importer {name} was not functional")
-                continue
-            
+        name = importer_class.__name__.title()
+        try:
+            importer = importer_class()
+        except Exception:
+            logging.warning(f"Importer {name} was not functional")
+        else:
             create_background_task(
                 id=f"Importing {name}",
                 function=importer.load,
                 retry_after=importer.time_between_runs
             )
-    except Exception as e:
-        logging.exception(e)
