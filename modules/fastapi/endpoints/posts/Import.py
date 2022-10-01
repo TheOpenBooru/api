9d67591ba6809a2a91fd6c9cf0e1c8770447f483
Ben Brady <benbradybusiness@gmail.com>
2022-10-01 20:56:23 +0100

    Fixed posts Import user_id

--- modules/fastapi/endpoints/posts/Import.py
+++ modules/fastapi/endpoints/posts/Import.py
@@ -18,7 +18,7 @@ from fastapi.responses import PlainTextResponse
 )
 async def import_url(url:str = Body(...), account:DecodeToken = Depends()):
     try:
-        new_posts = await posts.Import(url, account.id)
+        new_posts = await posts.Import(url, account.user_id)
     except posts.PostImportFailure as e:
         raise HTTPException(400, e.message)
     except Exception:
