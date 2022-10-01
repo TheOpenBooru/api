92654699d44cd34f88556360500d8eb1115c4ba7
Ben Brady <benbradybusiness@gmail.com>
2022-10-01 20:56:07 +0100

    Renamed Tag Regeneration Progress Bar

--- modules/database/Tag/regenerate.py
+++ modules/database/Tag/regenerate.py
@@ -13,7 +13,7 @@ def regenerate_count(min_count:int = settings.TAGS_MINIMUM_COUNT):
     start = time.time()
 
     tag_data = getTagData(min_count)
-    for doc in tqdm(tag_data,"Regenerating Tags"):
+    for doc in tqdm(tag_data,"Calculating Tag Counts"):
         name, count = doc
         
         if not exists(name):
