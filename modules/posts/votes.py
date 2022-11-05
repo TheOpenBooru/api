7f9fc4d1812c87ba681b443e7da438640ec169a8
Ben Brady <benbradybusiness@gmail.com>
2022-11-05 17:19:27 +0000

    Updated posts.votes

--- modules/posts/votes.py
+++ modules/posts/votes.py
@@ -1,49 +1,53 @@
 from modules.database import User, Post
 
+
 def add_upvote(post_id:int, user_id:int):
-    _add_upvote(user_id, post_id)
-    
     user = User.get(user_id)
+    
+    if id not in user.upvotes:
+        db_add_upvote(user_id, post_id)
+    if id in user.downvotes:
+        db_remove_downvote(user.id, post_id)
+
+
+def add_downvote(post_id:int, user_id:int):
+    user = User.get(user_id)
+    
+    if id not in user.downvotes:
+        db_add_downvote(user_id, post_id)
     if id in user.upvotes:
-        _remove_downvote(user.id, post_id)
+        db_remove_upvote(user.id, post_id)
 
 
 def remove_upvote(post_id:int, user_id:int):
     user = User.get(user_id)
-    if id in user.upvotes:
-        _remove_upvote(user.id, post_id)
-
-
-def add_downvote(post_id:int, user_id:int):
-    _add_downvote(user_id, post_id)
     
-    user = User.get(user_id)
     if id in user.upvotes:
-        _remove_upvote(user.id, post_id)
+        db_remove_upvote(user.id, post_id)
 
 
 def remove_downvote(post_id:int, user_id:int):
     user = User.get(user_id)
+    
     if id in user.downvotes:
-        _remove_downvote(user.id, post_id)
+        db_remove_downvote(user.id, post_id)
 
 
-
-def _add_upvote(user_id:int, post_id:int):
+def db_add_upvote(user_id:int, post_id:int):
     Post.add_upvote(post_id)
     User.add_upvote(user_id, post_id)
 
 
-def _remove_upvote(user_id:int, post_id:int):
+def db_remove_upvote(user_id:int, post_id:int):
     Post.remove_upvote(post_id)
     User.remove_upvote(user_id, post_id)
 
 
-def _add_downvote(user_id:int, post_id:int):
+def db_add_downvote(user_id:int, post_id:int):
     Post.add_downvote(post_id)
     User.add_downvote(user_id, post_id)
 
 
-def _remove_downvote(user_id:int, post_id:int):
+def db_remove_downvote(user_id:int, post_id:int):
     Post.remove_downvote(post_id)
     User.remove_downvote(user_id, post_id)
