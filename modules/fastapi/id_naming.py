bdc6d6ef189795cd0782516492f494ede7cc9dbc
Ben Brady <benbradybusiness@gmail.com>
2022-11-05 17:19:38 +0000

    Changed id_naming scheme

--- modules/fastapi/id_naming.py
+++ modules/fastapi/id_naming.py
@@ -1,8 +1,4 @@
 from fastapi.routing import APIRoute
 
 def generate_unique_id(route: APIRoute):
-    dirs = route.path.split("/")
-    if len(dirs) == 1:
-        return "index"
-    else:
-        return "_".join(dirs[1:])
+    return route.name
