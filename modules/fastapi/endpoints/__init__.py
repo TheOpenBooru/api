61e99515f6b584f91ce8ddbdc2a289d8d5535472
Ben Brady <benbradybusiness@gmail.com>
2022-12-17 11:27:54 +0000

    Fixed Tags import

--- modules/fastapi/endpoints/__init__.py
+++ modules/fastapi/endpoints/__init__.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter
 
 router = APIRouter()
 from . import media, status, index
-from . import account, profile, posts, post, tag, subscription
+from . import account, profile, posts, post, tags, subscription
 
 router.include_router(subscription.router)
 router.include_router(account.router)
 router.include_router(profile.router)
 router.include_router(posts.router)
 router.include_router(post.router)
-router.include_router(tag.router)
+router.include_router(tags.router)
