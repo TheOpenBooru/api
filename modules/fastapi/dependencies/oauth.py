b785fca8cc7324e8a35e51cef60d38d3d150f539
Ben Brady <benbradybusiness@gmail.com>
2022-12-26 20:12:11 +0000

    Changed _ Import

--- modules/fastapi/dependencies/oauth.py
+++ modules/fastapi/dependencies/oauth.py
@@ -1,10 +1,10 @@
-from fastapi.security import OAuth2PasswordBearer as _OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer
 from modules.schemas import UserPermissions
 
 all_perms = UserPermissions()
 scopes = {x:"" for x in all_perms.dict()}
 
-oauth2_scheme = _OAuth2PasswordBearer(
+oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl="/account/login",
     auto_error=False, # Allow for empty credentials
     scopes=scopes,
