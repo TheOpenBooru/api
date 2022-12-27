a8fb80cdac64d0a2f55467f766a10dfc45c0b742
Ben Brady <benbradybusiness@gmail.com>
2022-12-27 22:28:09 +0000

    Added Honeypot

--- modules/fastapi/middleware/__init__.py
+++ modules/fastapi/middleware/__init__.py
@@ -1,10 +1,12 @@
 from .blacklist import BlacklistMiddleware
+from .honeypot import HoneypotMiddleware
 from starlette.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 
 middlewares = [
     Middleware(BlacklistMiddleware),
+    Middleware(HoneypotMiddleware),
     Middleware(GZipMiddleware),
     Middleware(
         CORSMiddleware,
